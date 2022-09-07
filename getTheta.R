37b9ca4ef6aa7013f0b188ebbb5380efc37940c4
Frederik Bach Trier <frederik.bach97@gmail.com>
2022-09-07 10:06:06 +0200

    now with p=2

--- getTheta.R
+++ getTheta.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(matlib)
 library(tictoc)
+library(pracma)
 library(alabama)
 
 # turn off scinumbers, clear console and memory
@@ -19,8 +20,8 @@ source('utils/matrix_functions.R')
 #############################################################################
 data <- readxl::read_excel('data/12072022_embig_data.xlsx', sheet = 'Returns') %>%
   mutate(Date = as.Date(Date)) %>%
-  select(-IG, -HY) %>%
-  filter(between(Date, as.Date('2000-01-04'), as.Date('2005-12-31')))
+  select(Date, IG, HY) %>%
+  filter(between(Date, as.Date('2010-01-04'), as.Date('2016-12-31')))
 
 x <- data %>% select(-Date) %>% as.matrix() %>% t()
 p <- n <- nrow(x) 
@@ -29,34 +30,26 @@ p <- n <- nrow(x)
 ### estimate parameters ###
 #############################################################################
 theta0 <- rep(0.05, p*(p-1)/2+p+2*n*p+(p-n)*n)
-pars <- length(theta0)
 
-ub = c(pi/2*rep(1,p*(p-1)/2),
-       100*rep(1,p),
-       100*rep(1,n*p),
-       100*rep(1,n*p))
+# set optimizer bounds for constrained optimization
+lb <- c(rep(1e-10,p*(p-1)/2), 
+        rep(1e-10,p), 
+        rep(1e-10,n*p), 
+        rep(1e-10,n*p), 
+        rep(1e-10,(p-n)*n))
 
+ub <- c(rep(pi/2, p*(p-1)/2), 
+        rep(100, p), 
+        rep(100, n*p), 
+        rep(100, n*p),
+        rep(0, (p-n)*n))
 
-hin <- function(x) {
-  # set lower bound (zero for all parameters)
-  h <- x
-  
-  # set upper bound (pi/2)
-  for(par in 1:(p*(p-1)/2)){
-    h[pars + par] <- pi/2 - x[par]
-  }
-  
-  # set upper bound (100)
-  for(par in (p*(p-1)/2+1):pars){
-    h[pars + par] <- 100 - x[par]
-  }
-  h
-}
+# solution
+solution <- fmincon(theta0, EigenARCH_loglikelihood,
+                    lb = lb,
+                    ub = ub,
+                    tol = 1e-8, 
+                    maxfeval = 3*10e4, 
+                    maxiter = 10e5)
 
-tic('alabama optimizer')
-ans <- constrOptim.nl(par=theta0, 
-                      fn=EigenARCH_loglikelihood, 
-                      hin=hin) 
-toc()
-
-
+pars <- EigenARCH_repar(p,n,solution$par)
